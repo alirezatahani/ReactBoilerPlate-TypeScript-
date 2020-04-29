4ad6c37969e249dec942e3e198ea407c982a83af
Alireza Tahani <alireza.tahani0@gmail.com>
2020-04-30 00:03:42 +0430

    change to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import createStore from "./store/createStore";
 import Routes from "./Routes";
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider} from "react-redux";
-import './App.css';
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import "./App.css";
 
-const {store, persistor} = createStore();
+const { store, persistor } = createStore();
 
-const App: React.FC = () => {
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Routes/>
-            </PersistGate>
-        </Provider>
-    );
-};
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
+}
 
 export default App;
