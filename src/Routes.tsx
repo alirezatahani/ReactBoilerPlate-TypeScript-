0a17d302553dd6771c62e3f7b427071e7e35570e
Alireza Tahani <alireza.tahani0@gmail.com>
2020-04-30 00:02:49 +0430

    change to function component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 
-export default class Routes extends React.Component {
-    render() {
-        return (
-            <React.Fragment/>
-        )
-    }
+function Routes() {
+  return <React.Fragment />;
 }
+export default Routes;
